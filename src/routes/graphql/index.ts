0eaefe4c3742f4b16636d1f6045bd02aa00c7075
wingedseraph <r89647@gmail.com>
2025-11-20 17:55:02 +0300

    feat: add users query

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -2,22 +2,45 @@ import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import {
   graphql,
+  GraphQLID,
   GraphQLList,
   GraphQLNonNull,
   GraphQLObjectType,
   GraphQLSchema,
 } from 'graphql';
-import { MemberType } from './types/member.types.js';
+import { MemberType, MemberTypeId } from './types/member.types.js';
 
 import { PrismaClient } from '@prisma/client';
+import { UserType } from './types/user.types.js';
+import { UUIDType } from './types/uuid.js';
 const prisma = new PrismaClient();
 
 export const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
     memberTypes: {
-      type: new GraphQLNonNull(new GraphQLList(MemberType)),
-      resolve: () => prisma.memberType.findMany(),
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(MemberType))),
+      resolve: async () => prisma.memberType.findMany(),
+    },
+    memberType: {
+      type: MemberType,
+      args: {
+        id: { type: new GraphQLNonNull(MemberTypeId) },
+      },
+      resolve: async (_, { id }: { id: string }) =>
+        prisma.memberType.findUnique({ where: { id } }),
+    },
+    users: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))),
+      resolve: async () => prisma.user.findMany(),
+    },
+    user: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+      },
+      resolve: async (_, { id }: { id: string }) =>
+        prisma.user.findUnique({ where: { id } }),
     },
   },
 });
