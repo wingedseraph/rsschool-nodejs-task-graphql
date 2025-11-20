9c219efaa20a7f1c58da9efcc193e27e59aa6f23
wingedseraph <r89647@gmail.com>
2025-11-20 17:46:08 +0300

    feat: implement ProfileType query

---
+++ src/routes/graphql/types/user.types.ts
@@ -0,0 +1,41 @@
+import {
+  GraphQLBoolean,
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql';
+import { UUIDType } from './uuid.js';
+import { MemberType } from './member.types.js';
+
+export const ProfileType = new GraphQLObjectType({
+  name: 'ProfileType',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
+    yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
+    memberType: { type: new GraphQLNonNull(MemberType) },
+  }),
+});
+
+export const PostType = new GraphQLObjectType({
+  name: 'PostType',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    content: { type: new GraphQLNonNull(GraphQLString) },
+  }),
+});
+
+export const UserType = new GraphQLObjectType({
+  name: 'UserType',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    balance: { type: new GraphQLNonNull(GraphQLFloat) },
+    profile: { type: ProfileType },
+    posts: { type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(PostType))) },
+  }),
+});
